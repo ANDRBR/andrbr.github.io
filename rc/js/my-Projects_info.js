31eccb5ba5b29f44f0e47dbb1415669f65386dce
ANDRBR <andrbr59@gmail.com>
2017-01-27 23:57:32 -0400

    v1.0.0.0
    So far the site just displays a slideshow of the apps on my github

---
+++ rc/js/my-Projects_info.js
@@ -0,0 +1,20 @@
+var projects= [
+	{
+		name: "PolyBank",
+		img: "rc/img/PolyBank_capt.png",
+		link: "https://andrbr.github.io/PolyBank",
+		description: "Sitio diseñado para servir de banco para el juego \"Monopolio\""
+	},
+	{
+		name: "ResCalc",
+		img: "rc/img/ResCalc_capt.png",
+		link: "https://github.com/ANDRBR/ResCalc",
+		description: "Calculadora de resistencias"
+	},
+	{
+		name: "AppInstaller",
+		img: "rc/img/AppInstaller_capt.png",
+		link: "https://github.com/ANDRBR/QtAppInstaller",
+		description: "Programa para abrir ejecutables (de preferencia instaladores) en secuencia"
+	}
+];
