1973097703a65ec7a4eb221cf3d98b9cbb3ef8a0
ANDRBR <andrbr59@gmail.com>
2017-02-27 21:22:14 -0400

    Download section added

---
+++ rc/js/downloads_info.js
@@ -0,0 +1,26 @@
+var downloads = [
+	{
+		name: "AppInstaller",
+		description: "Programa para abrir ejecutables (de preferencia instaladores) en secuencia",
+		types: [
+			{
+				type: "Windows",
+				links: [
+					"AppInstaller.zip"
+				]
+			}
+		]
+	},
+	{
+		name: "ResCalc",
+		description: "Calculadora de resistencias",
+		types: [
+			{
+				type: "Windows",
+				links: [
+					"ResCalc.zip"
+				]
+			}
+		]
+	}
+]
